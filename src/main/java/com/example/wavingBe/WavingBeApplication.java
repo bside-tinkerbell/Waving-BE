4cc0eb753b5171c5c9fd8bce23ba17e5e8f99a95
swrookie <dpdjflr@gmail.com>
2023-06-04 16:37:27 +0900

    Initial Upload

---
+++ src/main/java/com/example/wavingBe/WavingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.wavingBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavingBeApplication.class, args);
+	}
+
+}
