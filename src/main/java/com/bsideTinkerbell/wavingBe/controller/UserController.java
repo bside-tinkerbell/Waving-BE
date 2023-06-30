73e6f83f0e651fc3b21121e7b83aecbf97afd1bd
swrookie <dpdjflr@gmail.com>
2023-06-30 20:08:04 +0900

    회원 서비스 일부 개선

--- src/main/java/com/bsideTinkerbell/wavingBe/controller/UserController.java
+++ src/main/java/com/bsideTinkerbell/wavingBe/controller/UserController.java
@@ -1,45 +1,64 @@
 package com.bsideTinkerbell.wavingBe.controller;
 
-import com.bsideTinkerbell.wavingBe.domain.dto.PersonalAuthenticationRequestDto;
-import com.bsideTinkerbell.wavingBe.domain.dto.PersonalAuthenticationVerificationDto;
-import com.bsideTinkerbell.wavingBe.domain.dto.UserDto;
+import com.bsideTinkerbell.wavingBe.domain.dto.*;
 import com.bsideTinkerbell.wavingBe.service.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 
 /**
- * 회원 서비스 API REST 엔드포인트 제공을 위한 Controller
+ * 회원 서비스 API 엔드포인트 제공을 위한 Controller
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("v1/users")
 public class UserController {
     private final UserService userService;
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
-    @PostMapping("/join")
-    public void createUser(@RequestBody @Valid UserDto request)
-            throws Exception, NoSuchAlgorithmException, InvalidKeySpecException {
-        userService.createUser(request);
-//        int resp = 0;
-//        return ResponseEntity.status(HttpStatus.OK).body(resp);
-    }
-
+    /**
+     * 회원가입 중 인증번호 요청 받는 endpoint
+     * @param request
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     */
     @PostMapping("/authentication")
-    public void sendAuthenticationCode(@RequestBody @Valid PersonalAuthenticationRequestDto request) throws NoSuchAlgorithmException, InvalidKeyException {
-        userService.sendAuthenticationCode(request);
+    public ResponseEntity<ResponseDto> sendAuthenticationCode(
+            @RequestBody @Valid PersonalAuthenticationRequestDto request
+    ) throws NoSuchAlgorithmException, InvalidKeyException {
+        ResponseDto responseDto = userService.sendAuthenticationCode(request);
+        return ResponseEntity.status(HttpStatus.valueOf(responseDto.getCode())).body(responseDto);
     }
 
     @PostMapping("/authentication-confirm")
-    public String confirmAuthenticationCode(@RequestBody @Valid PersonalAuthenticationVerificationDto request) {
-        return userService.verifyAuthenticationCode(request);
+    public ResponseEntity<ResponseDto> confirmAuthenticationCode(
+            @RequestBody @Valid PersonalAuthenticationVerificationDto request
+    ) {
+        ResponseDto responseDto = userService.confirmAuthenticationCode(request);
+        return ResponseEntity.status(HttpStatus.valueOf(responseDto.getCode())).body(responseDto);
+    }
+
+    /**
+     * 회원가입 시 회원정보 저장하는 endpoint
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("/join")
+    public ResponseEntity<ResponseDto> createUser(@RequestBody @Valid UserDto request) {
+        ResponseDto responseDto = userService.createUser(request);
+        return ResponseEntity.status(HttpStatus.valueOf(responseDto.getCode())).body(responseDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDto> login(@RequestBody UserLoginRequestDto request) {
+        ResponseDto responseDto = userService.loginUser(request);
+        return ResponseEntity.status(HttpStatus.valueOf(responseDto.getCode())).body(responseDto);
     }
 }
--- src/main/java/com/bsideTinkerbell/wavingBe/service/UserService.java
+++ src/main/java/com/bsideTinkerbell/wavingBe/service/UserService.java
@@ -1,91 +1,64 @@
 package com.bsideTinkerbell.wavingBe.service;
 
-import com.bsideTinkerbell.wavingBe.domain.dto.PersonalAuthenticationRequestDto;
-import com.bsideTinkerbell.wavingBe.domain.dto.PersonalAuthenticationVerificationDto;
-import com.bsideTinkerbell.wavingBe.domain.dto.UserDto;
+import com.bsideTinkerbell.wavingBe.domain.dto.*;
 import com.bsideTinkerbell.wavingBe.domain.entity.LoginEntity;
 import com.bsideTinkerbell.wavingBe.domain.entity.PersonalAuthenticationEntity;
 import com.bsideTinkerbell.wavingBe.domain.entity.UserEntity;
 import com.bsideTinkerbell.wavingBe.repository.LoginRepository;
 import com.bsideTinkerbell.wavingBe.repository.PersonalAuthenticationRepository;
 import com.bsideTinkerbell.wavingBe.repository.UserRepository;
+import com.bsideTinkerbell.wavingBe.security.JwtService;
 import com.bsideTinkerbell.wavingBe.util.EndpointNaver;
 import com.google.gson.Gson;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.transaction.Transactional;
-//import org.apache.hc.core5.net.URIBuilder;
+import lombok.RequiredArgsConstructor;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.utils.Base64;
-import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.HttpException;
 import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
-//import java.net.URI;
-//import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final LoginRepository loginRepository;
     private final PersonalAuthenticationRepository personalAuthenticationRepository;
     private final RedisTemplate<String, String> redisTemplate;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-    public UserService(
-            UserRepository userRepository
-            , LoginRepository loginRepository
-            , PersonalAuthenticationRepository personalAuthenticationRepository
-            , RedisTemplate<String, String> redisTemplate
-    ) {
-        this.userRepository = userRepository;
-        this.loginRepository = loginRepository;
-        this.personalAuthenticationRepository = personalAuthenticationRepository;
-        this.redisTemplate = redisTemplate;
-    }
-
-    @Transactional
-    public void createUser(UserDto userDto) throws Exception, NoSuchAlgorithmException, InvalidKeySpecException {
-        UserEntity userEntity = userDto.toUserEntity();
-        this.userRepository.save(userEntity);
-        UserEntity userOptionalEntity = this.userRepository.findByUsername(userDto.getUsername()).orElse(null);
-        if (userOptionalEntity == null) {
-            throw new Exception("아이디가 존재하지 않습니다");
-        }
-        PersonalAuthenticationEntity selfAuthenticationEntity = userDto.toSelfAuthenticationEntity(userOptionalEntity.getUserId());
-        LoginEntity loginEntity = userDto.toLoginEntity(userOptionalEntity.getUserId());
-        this.personalAuthenticationRepository.save(selfAuthenticationEntity);
-        this.loginRepository.save(loginEntity);
-    }
-
-    public String makeSignature(String method, String timestamp, String accessKey, String secretKey, String url) throws NoSuchAlgorithmException, InvalidKeyException {
+    private String makeSignature(String method, String timestamp, String accessKey, String secretKey, String url) throws NoSuchAlgorithmException, InvalidKeyException {
         String space = " ";					// one space
         String newLine = "\n";				// new line
-        String message = new StringBuilder()
-                .append(method)
-                .append(space)
-                .append(url)
-                .append(newLine)
-                .append(timestamp)
-                .append(newLine)
-                .append(accessKey)
-                .toString();
-        System.out.println(message);
+        String message = method +
+                space +
+                url +
+                newLine +
+                timestamp +
+                newLine +
+                accessKey;
+//        System.out.println(message);
         SecretKeySpec signingKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
         Mac mac = Mac.getInstance("HmacSHA256");
         mac.init(signingKey);
@@ -95,86 +68,248 @@ public class UserService {
     }
 
     /**
+     * 10000 ~ 99999 사이 인증 코드 생성
+     * @return verificationCode
+     */
+    private int generateVerificationCode() {
+        return new Random().nextInt(90000) + 10000;
+    }
+
+    /**
+     *
+     * @param password 암호화 안된 비밀번호
+     * @return SHA512 암호화 된 비밀번호
+     * @throws NoSuchAlgorithmException 알고리즘 예외처리
+     */
+    private String encryptPasswordWithSHA512(String password) throws NoSuchAlgorithmException {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        md.update(salt);
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        return new String(hashedPassword);
+    }
+
+    /**
+     *
+     * @param username 사용자 아이디 (이메일)
+     * @param password 암호화 되지 않은 비밀번호
+     * @return 사용자
+     */
+    private UserEntity authenticate(String username, String password) {
+        UserEntity user = userRepository.findByUsername(username).orElse(null);
+        if (user == null)
+            return null;
+        LoginEntity login = loginRepository.findById(user.getUserId()).orElse(null);
+        if (login != null && passwordEncoder.matches(password, login.getSalt())) {
+            return user;
+        }
+        return null;
+    }
+
+//    /**
+//     *
+//     * @param user 사용자(회원) 객체
+//     * @return token: 로그인 인증을 위한 JWT
+//     */
+//    private String generateToken(UserEntity user) {
+//        String secretKey = "waving";
+//        // 1시간 유요시간 설정
+//        long expirationTime = 3600000;
+//
+//        return Jwts.builder()
+//                .setSubject(user.getUsername())
+//                .setIssuedAt(new Date())
+//                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+//                .signWith(SignatureAlgorithm.HS512, secretKey)
+//                .compact();
+//    }
+
+    /**
      * nCloud SENS 통해 휴대폰 인증번호 전송
      * @param personalAuthRequestDto 본인인증 데이터 전송 객체
      */
-    public void sendAuthenticationCode(PersonalAuthenticationRequestDto personalAuthRequestDto) throws NoSuchAlgorithmException, InvalidKeyException {
+    public ResponseDto sendAuthenticationCode(PersonalAuthenticationRequestDto personalAuthRequestDto) throws NoSuchAlgorithmException, InvalidKeyException {
         String method = "POST";                                         // method
         String timestamp = String.valueOf(System.currentTimeMillis());	// current timestamp (epoch)
         String accessKey = "Ims2TbuxYF9fPCg9G2n6";			            // access key id (from portal or Sub Account)
-        String secretKey = "8VwKPaIodjIu8YP1cTsW8KIKqkVJfaSBOBU7UwJF";
-
-        String baseUri = "https://sens.apigw.ntruss.com";
+        String secretKey = "8VwKPaIodjIu8YP1cTsW8KIKqkVJfaSBOBU7UwJF";  // secret key
+        String baseUri = "https://sens.apigw.ntruss.com";               // sens base uri
         String apiUri = EndpointNaver.SENS_SEND_SMS.getUrl() + "/services/ncp:sms:kr:309403274976:waving" + "/messages";
-        String xNcpApigwSignatureV2 = makeSignature(method, timestamp, accessKey, secretKey, apiUri);
-        System.out.println(method + " " + timestamp + " " + accessKey + " " + xNcpApigwSignatureV2);
-
+        String signature = makeSignature(method, timestamp, accessKey, secretKey, apiUri);
         String uri = baseUri + apiUri;
-        System.out.println(uri);
+        int verificationCode = generateVerificationCode();   // 인증코드 생성
+
+
+        Map<String, Object> requestBodyMap = new HashMap<>();
+        List<Map<String, String>> messagesList = new ArrayList<>();
+        Map<String, String> messagesMap = new HashMap<>();
+
+        requestBodyMap.put("type", "SMS");
+        requestBodyMap.put("contentType", "COMM"); // COMM: 일반 메시지, AD: 광고메시지
+        requestBodyMap.put("countryCode", "82");
+        requestBodyMap.put("from", "01020176384");  // TODO: 전화번호 web 발신 혹은 하드코딩 제거
+//            requestBodyMap.put("subject", "");    // LMS, MMS에서만 사용 가능, 최대 40byte
+        String content = "인증번호: " + verificationCode;
+        requestBodyMap.put("content", content);
+        messagesMap.put("to", personalAuthRequestDto.getCellphone());
+//        messagesMap.put("content", content);  // 개별 메시지 내용 SMS: 최대 80 byte, LMS, MMS: 최대 2000byte
+        messagesList.add(messagesMap);
+        requestBodyMap.put("messages", messagesList);
+//            requestBodyMap.put("files", "");  // MMS에서만 사용가능
+//            requestBodyMap.put("reserveTime", "");    // 메시지 발송 예약 일시 (yyyy-MM-dd HH:mm)
+//            requestBodyMap.put("reserveTimeZone", "");    // 예약 일시 타임존 (기본: Asia/Seoul)
+//            requestBodyMap.put("scheduleCode", "");       // 등록하려는 스케줄 코드
+        String requestBodyString = new Gson().toJsonTree(requestBodyMap).getAsJsonObject().toString();
+//        System.out.println(requestBodyString);
+
         HttpPost httpPost = new HttpPost(uri);
         httpPost.setHeader("Content-Type", "application/json; charset=utf-8");
         httpPost.setHeader("x-ncp-apigw-timestamp", timestamp);
         httpPost.setHeader("x-ncp-iam-access-key", accessKey);
-        httpPost.setHeader("x-ncp-apigw-signature-v2", xNcpApigwSignatureV2);
-        Gson gson = new Gson();
-        Random rand = new Random();
-        int authenticationCode = rand.nextInt(90000) + 10000;   // 인증코드 생성
-        final ValueOperations<String, String> stringStringValueOperations = redisTemplate.opsForValue();
-        stringStringValueOperations.set(personalAuthRequestDto.getCellphone(), Integer.toString(authenticationCode), 5, TimeUnit.MINUTES);
-
-        Map<String, Object> requestBodyMap = new HashMap<>();
-        requestBodyMap.put("type", "SMS");
-        requestBodyMap.put("contentType", "COMM"); // COMM: 일반 메시지, AD: 광고메시지
-        requestBodyMap.put("countryCode", "82");
-        requestBodyMap.put("from", "01020176384");
-//            requestBodyMap.put("subject", "");
-        String content = "인증번호: " + String.valueOf(authenticationCode);
-        requestBodyMap.put("content", content);
-        List<Map<String, String>> messagesList = new ArrayList<>();
-        Map<String, String> messagesMap = new HashMap<>();
-        messagesMap.put("to", personalAuthRequestDto.getCellphone());
-        messagesMap.put("content", content);
-        System.out.println(gson.toJsonTree(messagesMap).getAsJsonObject().toString());
-        messagesList.add(messagesMap);
-        requestBodyMap.put("messages", messagesList);
-//            requestBodyMap.put("files", "");
-//            requestBodyMap.put("reserveTime", "");
-//            requestBodyMap.put("reserveTimeZone", "");
-//            requestBodyMap.put("scheduleCode", "");
-        String requestBodyString = gson.toJsonTree(requestBodyMap).getAsJsonObject().toString();
-        System.out.println(requestBodyString);
+        httpPost.setHeader("x-ncp-apigw-signature-v2", signature);
         httpPost.setEntity(new StringEntity(requestBodyString));
-        HttpClientResponseHandler<CloseableHttpResponse> handler = new HttpClientResponseHandler<CloseableHttpResponse>() {
-            @Override
-            public CloseableHttpResponse handleResponse(ClassicHttpResponse response) throws HttpException, IOException {
-                return (CloseableHttpResponse) response;
-            }
-        };
+
+        ResponseDto responseDto = new ResponseDto();
+        Map<String, Object> result = new HashMap<>();
 
         try(CloseableHttpClient client = HttpClients.createDefault()) {
-            CloseableHttpResponse response = client.execute(httpPost, handler);
-            HttpEntity entity = response.getEntity();
-            System.out.println("response: " + EntityUtils.toString(entity));
+            client.execute(httpPost, response -> {
+                if (response.getCode() == 202) {
+                    redisTemplate.opsForValue().set(
+                            personalAuthRequestDto.getCellphone()
+                            , Integer.toString(verificationCode)
+                            , 5
+                            , TimeUnit.MINUTES
+                    );
+                    responseDto.setCode(200);
+                    result.put("message", "success");
+                    responseDto.setResult(result);
+                }
+                System.out.println(response.getCode() + " " + response.getReasonPhrase());
+                final HttpEntity entity = response.getEntity();
+                String responseBodyString = EntityUtils.toString(entity);
+                System.out.println(responseBodyString);
+                EntityUtils.consume(entity);
+
+                return null;
+            });
         } catch (Exception ex) {
-            System.out.println("error occurred: " + ex.toString());
+            System.out.println("error occurred: " + ex);
+
+            responseDto.setCode(400);
+            result.put("message", "error occurred");
+            responseDto.setResult(result);
         }
+
+        return responseDto;
     }
 
-    public String verifyAuthenticationCode(PersonalAuthenticationVerificationDto personalAuthVerificationDto) {
-        final String cellphone = personalAuthVerificationDto.getCellphone();
-        final ValueOperations<String, String> stringStringValueOperations = redisTemplate.opsForValue();
-        final String stringCode = stringStringValueOperations.get(cellphone);
+    public ResponseDto confirmAuthenticationCode(PersonalAuthenticationVerificationDto personalAuthVerificationDto) {
+        ResponseDto responseDto = new ResponseDto();
+        Map<String, Object> result = new HashMap<>();
 
-        if (stringCode == null)
-            return "Verification Code Expired";
-        else {
-            final int code = Integer.parseInt(stringCode);
-            if (code == personalAuthVerificationDto.getCode()) {
-                redisTemplate.opsForValue().getAndDelete(cellphone);
-                return "Verification Success";
+        try {
+            String cellphone = personalAuthVerificationDto.getCellphone();
+            String stringCode = redisTemplate.opsForValue().get(cellphone);
+
+            if (stringCode == null) {
+                responseDto.setCode(404);
+                result.put("message", "verification code expired");
             }
-            else
-                return "Invalid Verification Code";
+            else {
+                final int code = Integer.parseInt(stringCode);
+                if (code == personalAuthVerificationDto.getCode()) {
+                    redisTemplate.opsForValue().getAndDelete(cellphone);
+                    responseDto.setCode(200);
+                    result.put("message", "success");
+                } else {
+                    responseDto.setCode(404);
+                    result.put("message", "invalid verification code");
+                }
+            }
+        } catch (Exception ex) {
+            responseDto.setCode(400);
+            result.put("message", ex);
         }
+        responseDto.setResult(result);
+
+        return responseDto;
+    }
+
+    /**
+     *
+     * @param userDto 사용자 정보
+     * @return 요청 응답
+     * @throws Exception 예외처리
+     */
+    @Transactional
+    public ResponseDto createUser(UserDto userDto) {
+        ResponseDto responseDto = new ResponseDto();
+        Map<String, Object> result = new HashMap<>();
+        try {
+            UserEntity userEntity = userDto.toUserEntity();
+            String password = userDto.getPassword();
+            this.userRepository.save(userEntity);
+
+            // 회원 테이블 저장 후 회원 아이디 조회
+            UserEntity userOptionalEntity = this.userRepository.findByUsername(userDto.getUsername()).orElse(null);
+            if (userOptionalEntity == null) {
+                throw new Exception("아이디가 존재하지 않습니다");
+            }
+
+            // 조회 한 회원 아이디 기준으로 본인인증, 로그인 정보 저장
+            PersonalAuthenticationEntity personalAuthenticationEntity = userDto.toSelfAuthenticationEntity(
+                    userOptionalEntity.getUserId());
+            LoginEntity loginEntity = userDto.toLoginEntity(
+                    userOptionalEntity.getUserId()
+                    , passwordEncoder.encode(password)
+                    , encryptPasswordWithSHA512(password)
+            );
+            this.personalAuthenticationRepository.save(personalAuthenticationEntity);
+            this.loginRepository.save(loginEntity);
+
+            responseDto.setCode(200);
+            result.put("message", "success");
+            responseDto.setResult(result);
+        } catch (Exception ex) {
+            responseDto.setCode(400);
+            result.put("message", ex);
+            responseDto.setResult(result);
+        }
+
+        return responseDto;
+    }
+
+    public ResponseDto loginUser(UserLoginRequestDto userLoginRequestDto) {
+        ResponseDto responseDto = new ResponseDto();
+        Map<String, Object> result = new HashMap<>();
+
+        String username = userLoginRequestDto.getUsername();
+        String password = userLoginRequestDto.getPassword();
+
+        // 아이디(이메일), 비번 둘중 하나라도 없으면 에러 반환
+        if (username == null || password == null) {
+            responseDto.setCode(400);
+            result.put("message", "username and password are required");
+            return responseDto;
+        }
+
+        // 사용자 인증
+        UserEntity user = authenticate(username, password);
+        if (user != null) {
+            String token = jwtService.generateToken(user);
+            responseDto.setCode(200);
+            result.put("token", token);
+        }
+        else {
+            responseDto.setCode(401);
+            result.put("message", "invalid username or password");
+        }
+        responseDto.setResult(result);
+
+        return responseDto;
     }
 }
